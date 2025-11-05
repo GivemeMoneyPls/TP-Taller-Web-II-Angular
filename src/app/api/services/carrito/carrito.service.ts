e703799b4aeef134bfd0aa4483428a9f8e016ed0
NadiaSpi <nadia.belen98@gmail.com>
2025-11-05 10:09:17 -0300

    Parte back de agregar productos al carrito del usuario

---
+++ src/app/api/services/carrito/carrito.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarritoItem, CarritoResponse } from '../../../modules/carrito/interfaces/carrito.interface';
+import { environment } from '../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoService {
+  httpClient = inject(HttpClient);
+
+  agregarAlCarrito(juegoId: number, usuarioId: number): Observable<CarritoResponse> {
+  return this.httpClient.post<CarritoResponse>(
+    `${environment.API_URL}/carrito/agregar`, 
+    { juegoId, usuarioId }
+  );
+}
+}

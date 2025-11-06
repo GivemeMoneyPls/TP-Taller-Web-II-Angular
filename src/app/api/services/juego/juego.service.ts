aea1b83f006832cbc5c8d47913aff5d0c4685f22
Sofia <sofiapulice13@gmail.com>
2025-11-06 03:52:38 -0300

    juegos similares por genero

--- src/app/api/services/juego/juego.service.ts
+++ src/app/api/services/juego/juego.service.ts
@@ -19,4 +19,11 @@ export class JuegoService {
     return this.httpClient.get<Juego>(`${environment.API_URL_GAME}${id}`);
   }
 
+  getJuegosSimilares(generos: string[], idActual: number): Observable<Juego[]> {
+    const generosParam = generos.join(',');
+    return this.httpClient.get<Juego[]>(
+      `${environment.API_URL_GAME}similares/${idActual}?generos=${generosParam}`
+    );
+}
+
 }

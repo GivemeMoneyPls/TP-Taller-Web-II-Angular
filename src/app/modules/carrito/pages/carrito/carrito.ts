e5d6a00408b46673382d61d6b74945c2a410cf3c
NadiaSpi <nadia.belen98@gmail.com>
2025-11-07 10:43:15 -0300

    total y subtotal carrito

--- src/app/modules/carrito/pages/carrito/carrito.ts
+++ src/app/modules/carrito/pages/carrito/carrito.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurrencyPipe } from '@angular/common';
+import { Router } from '@angular/router';
 import { CarritoService } from '../../../../api/services/carrito/carrito.service';
 import { CarritoItem } from '../../interfaces/carrito.interface';
 import { GenerosPipe } from '../../../../modules/juegos/pipes/generos-pipe';
+
 @Component({
   selector: 'app-carrito',
   imports: [CommonModule, CurrencyPipe, GenerosPipe],
@@ -11,24 +13,30 @@ import { GenerosPipe } from '../../../../modules/juegos/pipes/generos-pipe';
   styleUrl: './carrito.css',
 })
 export class Carrito {
-
   juegos: CarritoItem[] = [];
   mensaje: string | null = null;
+  total: number = 0; 
 
   carritoService = inject(CarritoService);
+  router = inject(Router);
 
   ngOnInit(): void {
-    this.obtenerCarrito();
-  }
+    const usuario = JSON.parse(localStorage.getItem('app_user') || '{}');
 
   
-  obtenerCarrito(): void {
-    const usuario = JSON.parse(localStorage.getItem('app_user') || '{}');
-    const usuarioId = usuario.id;
+    if (!usuario.id) {
+      this.router.navigate(['/signin']);
+      return;
+    }
 
+    this.obtenerCarrito(usuario.id);
+  }
+
+  obtenerCarrito(usuarioId: number): void {
     this.carritoService.getCarrito(usuarioId).subscribe({
       next: (carrito) => {
         this.juegos = carrito;
+        this.calcularTotal(); // 💰 Calcula total al cargar
         console.log('Carrito obtenido:', this.juegos);
       },
       error: (error) => {
@@ -36,9 +44,15 @@ export class Carrito {
       },
       complete: () => {
         console.log('Carga del carrito completada.');
-      }
+      },
     });
   }
 
  
+  calcularTotal(): void {
+    this.total = this.juegos.reduce((sum, item) => {
+      const precio = Number(item.juego.precio) || 0;
+      return sum + precio * item.cantidad;
+    }, 0);
+  }
 }

28503e67c9fcbec2fb0a5330274fe5ae9f99e10e
NickiQuaranta <nicki.quaranta@gmail.com>
2025-11-07 23:13:52 -0300

    mis pedidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DetailJuegos } from './modules/juegos/pages/detail-juegos/detail-juegos
 import { Home } from './public/home/home';
 import { SignupComponent } from './modules/auth/pages/signup/signup';
 import { SigninComponent } from './modules/auth/pages/signin/signin';
+import { MisComprasComponent } from './modules/mis-compras/mis-compras.component';
 
 export const routes: Routes = [
 
@@ -27,8 +28,14 @@ export const routes: Routes = [
     component: DetailJuegos
   },
 
+   { path: 'mis-compras', 
+      component: MisComprasComponent 
+  },
+
     {
       path: '**',
       redirectTo: ''
-    }
+    },
+
+  
 ];
